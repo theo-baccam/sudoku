fb758c9a75d9e607fb4cfbb5ecf5caa6b0ea67fd
Chaima BEKHAOUDA <137110936+chaima-bekhaouda@users.noreply.github.com>
2024-05-14 17:48:26 +0200

    Add Interface.cpp with basic window setup for Sudoku game

---
+++ src/Interface.cpp
@@ -0,0 +1,26 @@
+#include "raylib.h"
+
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "sudoku window");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Sudoku!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
