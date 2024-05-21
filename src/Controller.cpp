5dda87b9f6113300d7ec3ca2832bc9da13d11dc5
Chaima BEKHAOUDA <137110936+chaima-bekhaouda@users.noreply.github.com>
2024-05-21 14:52:54 +0200

    docs: add comments to clarify code functionality

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -6,24 +6,35 @@
 #include <iostream>
 
 
+// Constructor of the Controller class
 Controller::Controller() {
+    // Generate a Sudoku board with 40% of the cells filled
     board.generateBoard(0.40);
 }
+// Destructor of the Controller class
 Controller::~Controller() {}
 
+// Method to run the game
 void Controller::run() {
+    // Initialize a window with dimensions 800x800 and title "Sudoku"
     InitWindow(800, 800, "Sudoku");
 
+    // Loop until the window should close
     while(!WindowShouldClose()) {
+        // Check if a number key is pressed
         int pressedNumber = window.getPressedNumber();
         if (pressedNumber != -1) {
+             // Get the coordinates of the hovered square
             const int y = window.getHoveredSquareY();
             const int x = window.getHoveredSquareX();
 
+            // If the original Sudoku board at the hovered square is empty
             if (board.originalBoard[y][x] == 0) {
+                 // Set the value of the Sudoku board at the hovered square to the pressed number
                 board.sudokuBoard[y][x] = pressedNumber;
             }
         };
+        // Draw the Sudoku board
         window.drawSudokuBoard(board.sudokuBoard, board.originalBoard);
     };
 
